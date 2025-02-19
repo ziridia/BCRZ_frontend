f64b496d75ab971194e2c8758b94d342af07c372
Raj-Shukla2002 <90229851+Raj-Shukla2002@users.noreply.github.com>
2025-02-18 21:18:16 -0500

    Add files via upload

--- transaction_managers/paybill_manager.py
+++ transaction_managers/paybill_manager.py
@@ -1,24 +1,84 @@
 
-
 from transaction_manager import TransactionManager
 
 class states:
     beforePaybill = 0 # user just typed "withdrawal", display appropriate message
     transactionExit = -1 # flag transaction as finished (error or successful completion)
+    awaitAccountName = 1
+    awaitAccountNumber = 2
+    awaitReciever = 3
+    awaitAmount = 4
 
 
 class PaybillManager(TransactionManager):
+    """Company examples for bill payments, using reciever codes for company codes."""
+
+    Company_Examples = {"EC": "The Bright Light Electric Company",
+                        "CQ": "Credit Card Company Q",
+                        "FI": "Fast Internet, Inc."}
 
     def __init__(self, user):
         self.user = user    
         self.state:int = states.beforePaybill
+        self.account_name = None
+        self.account_number = None
+        self.reciever_index = None
+        self.amount = None
+        self.accountBalance = 1000
+        """Logging transactions so they can be printed."""
+        self.transactions = []
+    
 
     def next(self, user_input):
-
+        
         if self.state == states.beforePaybill:
+            self.state = states.awaitAccountName
+            return "Enter account name: "
+        
+        elif self.state == states.awaitAccountName:
+            self.state = states.awaitAccountNumber
+            return "Enter account number: "
+        
+        elif self.state == states.awaitAccountNumber:
+            # Verify if account exists
 
-            self.state = states.transactionExit
-            return "message"
+            # Next Part of the transaction.
+            self.account_number = user_input
+            self.state = states.awaitReciever
+            return "Enter Reciever code (EC, CQ, FI): "
+
+        elif self.state == states.awaitReciever:
+            # Verify if Reciever code is a vaild code.
+            if user_input.strip().upper() not in self.Company_Examples:
+                return "Error: Invaild Reciever code"
+            
+            else:
+                # Next part of the transaction.
+                self.reciever_index = user_input.strip().upper()
+                self.state = states.awaitAmount
+                return "Enter amount for bill payment: "
+        
+        elif self.state == states.awaitAmount:
+            # Check if amount is valid and proceed with the transaction.
+            try:
+                self.amount = float(user_input)
+
+                if self.amount < 0 or self.amount > 2000:
+                    return f"Error: Invalid amount. Must be between 0 and 2000. Inputed: {self.amount}"
+                
+                # Example for exceeding account balance:
+                if self.amount > self.accountBalance:
+                    return f"Error: Invalid amount. Exceeds account balance. Inputed: {self.amount}"
+                
+                # Recording transaction for logs
+                #transaction = f"03_{self.account_name}_{self.account_number}_{self.amount}_{self.reciever_index}"
+                #self.transactions.append(transaction)
+
+                self.state = states.transactionExit
+                return f"Bill Payment of {self.amount} to {self.Company_Examples[self.reciever_index]} completed."
+
+            except ValueError:
+                return f"Error: Invalid input. Please enter a numeric value and not {self.amount}"
         
         return "error: state machine is not exiting properly"
 
--- transaction_managers/transfer_manager.py
+++ transaction_managers/transfer_manager.py
@@ -5,6 +5,10 @@ from transaction_manager import TransactionManager
 class states:
     beforeTransfer = 0 # user just typed "withdrawal", display appropriate message
     transactionExit = -1 # flag transaction as finished (error or successful completion)
+    awaitAccountName = 1
+    awaitAccountNumber = 2
+    awaitSecondAccountNumber = 3
+    awaitAmount = 4
 
 
 class TransferManager(TransactionManager):
@@ -12,15 +16,65 @@ class TransferManager(TransactionManager):
     def __init__(self, user):
         self.user = user    
         self.state:int = states.beforeTransfer
+        self.account_name = None
+        self.account_number = None
+        self.second_account_number = None
+        self.amount = None
+        # For testing account balance
+        self.accountBalance = 800
+        # Logging transfers so they can be printed.
+        self.transfers = []
 
     def next(self, user_input):
 
-        if self.state == states.beforeTransfer:
 
-            self.state = states.transactionExit
-            return "message"
+            if self.state == states.beforeTransfer:
+                self.state = states.awaitAccountName
+                return "Enter account name: "
+            
+            elif self.state == states.awaitAccountName:
+                self.state = states.awaitAccountNumber
+                return "Enter account number: "
+            
+            elif self.state == states.awaitAccountNumber:
+                # Verify if account exists
+
+                # Next Part of the Transfer.
+                self.account_number = user_input
+                self.state = states.awaitReciever
+                return "Enter reciever of transfer account number: "
+
+            elif self.state == states.awaitReciever:
+                # Verify if Reciever Account is a vaild code.
+                
+                # Next part of the Transfer.
+                self.second_account_number = user_input.strip().upper()
+                self.state = states.awaitAmount
+                return "Enter amount for bill payment: "
+            
+            elif self.state == states.awaitAmount:
+                # Check if amount is valid and proceed with the Transfer.
+                try:
+                    self.amount = float(user_input)
+
+                    if self.amount < 0 or self.amount > 1000:
+                        return f"Error: Invalid amount. Must be between 0 and 1000. Inputed: {self.amount}"
+                    
+                    # Example for exceeding account balance:
+                    if self.amount > self.accountBalance:
+                        return f"Error: Invalid amount. Exceeds account balance. Inputed: {self.amount}"
+                    
+                    # Recording Transfer for logs
+                    #transfer = f"02_{self.account_name}_{self.account_number}_{self.amount}_{self.second_account_number}"
+                    #self.transfer.append(transfer)
+
+                    self.state = states.transactionExit
+                    return f"Transfer of {self.amount} to {self.second_account_number} completed."
+
+                except ValueError:
+                    return f"Error: Invalid input. Please enter a numeric value and not {self.amount}"
         
-        return "error: state machine is not exiting properly"
+            return "error: state machine is not exiting properly"
 
 
     def isComplete(self):
